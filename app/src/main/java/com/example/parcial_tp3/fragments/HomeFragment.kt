fdbde3afb0e098a78e0bffcf94fd613da3421375
svalino95 <santiago.valino@gmail.com>
2022-11-08 21:40:50 -0300

    first activity home

---
+++ app/src/main/java/com/example/parcial_tp3/fragments/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.parcial_tp3.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.parcial_tp3.R
+
+class HomeFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_fragment)
+    }
+}
